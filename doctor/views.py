3b615da9acf04f7088dc0bec99945595bc861c2e
Sobuj <srsobuj150921@gmail.com>
2024-11-16 15:51:44 +0600

    Medical Record Added for Patient

--- doctor/views.py
+++ doctor/views.py
@@ -1,13 +1,23 @@
 from django.shortcuts import render
-from .models import Doctor,Designation,Specialization,AvailableTime,Review
-from .serializers import DesignationSerializer,DoctorSerializer,SpecializationSerializer,AvailableTimeSerializer,ReviewSerializer
+from .models import Doctor,Designation,Specialization,AvailableTime,Review,MedicalRecord
+from .serializers import DesignationSerializer,DoctorSerializer,SpecializationSerializer,AvailableTimeSerializer,ReviewSerializer,MedicalRecordSerializer
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticatedOrReadOnly,IsAuthenticated
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.filters import SearchFilter,BaseFilterBackend
 # Create your views here.
+
+class DoctorPagination(PageNumberPagination):
+    page_size = 1
+    page_size_query_param = 'page_size'
+    max_page_size = 10
+
 
 class DoctorView(viewsets.ModelViewSet):
     queryset=Doctor.objects.all()
     serializer_class=DoctorSerializer
+    pagination_class=DoctorPagination
+    filter_backends=[SearchFilter]
     
 class SpecializationView(viewsets.ModelViewSet):
     queryset=Specialization.objects.all()
@@ -17,11 +27,28 @@ class DesignationView(viewsets.ModelViewSet):
     queryset=Designation.objects.all()
     serializer_class=DesignationSerializer
     
+class AvailableTimeForSpecificDoctor(BaseFilterBackend):
+    def filter_queryset(self, request, queryset, view):
+        doctor_id=request.query_params.get('doctor_id')
+        if doctor_id:
+            return queryset.filter(doctor=doctor_id)
+        return queryset
+            
+
 class AvailableTimeView(viewsets.ModelViewSet):
+    permission_classes=[IsAuthenticatedOrReadOnly]
     queryset=AvailableTime.objects.all()
     serializer_class=AvailableTimeSerializer
+    filter_backends=[AvailableTimeForSpecificDoctor]
     
 class ReviewView(viewsets.ModelViewSet):
     permission_classes=[IsAuthenticated]
     queryset=Review.objects.all()
     serializer_class=ReviewSerializer
+    
+    
+class MedicalRecordView(viewsets.ModelViewSet):
+    queryset=MedicalRecord.objects.all()
+    serializer_class=MedicalRecordSerializer
+    
+    
--- patients/models.py
+++ patients/models.py
@@ -4,11 +4,21 @@ from django.contrib.auth import get_user_model
 from django.conf import settings
 # Create your models here.
 User = get_user_model()
+
+GENDER_CHOICES=(
+    ('Male','Male'),
+    ('Female','Female'),
+    ('other','other'),
+)
 class Patient(models.Model):
     user=models.OneToOneField(User,on_delete=models.CASCADE)
-    mobile_no=models.CharField(max_length=11)
+    mobile_no=models.CharField(max_length=11,unique=True)
+    address=models.TextField(default='Dhaka,Bangladesh')
+    country=models.CharField(max_length=50,default='Bangladesh')
+    date_of_birth=models.DateField(null=True, blank=True)
+    gender=models.CharField(max_length=15,choices=GENDER_CHOICES,default='Male')
     image=models.ImageField(upload_to='patients/images/')
     
     def __str__(self):
-        return f"{self.user.first_name}{self.user.last_name}"
+        return f"{self.user.first_name}{self.user.last_name} {self.user.email}"
     
